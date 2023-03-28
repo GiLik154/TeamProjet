0897d8b8d88fd493cad7d3c78c87aac158f746c2
Choisehun <wjsrkqeovh@naver.com>
2023-03-28 10:01:35 +0900

    판매처 상품 Entity 정규화 1차본

---
+++ src/test/java/com/example/team_project/shop/service/ShopJoinServiceTest.java
@@ -0,0 +1,63 @@
+package com.example.team_project.shop.service;
+
+import com.example.team_project.domain.shop.domain.Seller;
+import com.example.team_project.domain.shop.domain.SellerRepository;
+import com.example.team_project.domain.shop.domain.Shop;
+import com.example.team_project.domain.shop.domain.ShopRepository;
+import com.example.team_project.domain.shop.service.SellerJoinService;
+import com.example.team_project.domain.shop.service.ShopJoinService;
+import com.example.team_project.domain.shop.service.dto.ShopJoinDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@Transactional
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+ class ShopJoinServiceTest {
+    private final SellerRepository sellerRepository;
+    private final SellerJoinService sellerJoinService;
+    private final ShopRepository shopRepository;
+    private final ShopJoinService shopJoinService;
+
+
+    @Autowired
+    ShopJoinServiceTest(SellerJoinService sellerJoinService,
+                        SellerRepository sellerRepository,
+                        ShopRepository shopRepository,
+                        ShopJoinService shopJoinService
+    ) {
+        this.sellerJoinService = sellerJoinService;
+        this.sellerRepository = sellerRepository;
+        this.shopJoinService = shopJoinService;
+        this.shopRepository = shopRepository;
+    }
+
+    @Test
+    void 판매자_정상_등록() {
+        Seller seller = new Seller("testId", "testPw", "testName", "testPhone");
+        sellerRepository.save(seller);
+
+        //가게등록을 따로함
+        ShopJoinDto shopJoinDto = new ShopJoinDto(
+                "testshopname",
+                "testshopaddress",
+                "testregnumber"
+        );
+
+        shopJoinService.shopJoin(seller.getId(), shopJoinDto);
+
+        Shop shop = shopRepository.findByShopName("testshopname");
+
+        assertEquals(seller, shop.getSeller());
+        assertEquals("testshopname", shop.getShopName());
+        assertEquals("testshopaddress", shop.getShopAddress());
+        assertEquals("testregnumber", shop.getBusinessRegistrationNumber());
+    }
+
+
+}
