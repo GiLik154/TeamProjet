6939f95f2bf2d8d0d8b0071735e5f0226eb9f17a
seoljunlim <seoljunlim@gmail.com>
2023-03-28 11:33:43 +0900

    feat Post 엔티티 생성

---
+++ src/main/java/com/example/team_project/domain/post/domain/PostRepository.java
@@ -0,0 +1,6 @@
+package com.example.team_project.domain.post.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post,Long> {
+}
