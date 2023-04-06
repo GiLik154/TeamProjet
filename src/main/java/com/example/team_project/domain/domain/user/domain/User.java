e0e12c6f2faf434382a9f1c089da2b1af93ace5f
rigel9327 <rigel9327@gmail.com>
2023-04-07 04:04:48 +0900

    중간 저장

--- src/main/java/com/example/team_project/domain/domain/user/domain/User.java
+++ src/main/java/com/example/team_project/domain/domain/user/domain/User.java
@@ -2,6 +2,7 @@ package com.example.team_project.domain.domain.user.domain;
 
 import com.example.team_project.enums.UserGrade;
 import lombok.Getter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.persistence.*;
 
@@ -28,11 +29,26 @@ public class User {
     @Column(nullable = false)
     private UserGrade userGrade;
 
+    public boolean isEncodePassword(PasswordEncoder passwordEncoder, String password) {
+        this.password = passwordEncoder.encode(password);
+        return passwordEncoder.matches(password, this.password);
+    }
+
+    public boolean isValidPassword(PasswordEncoder passwordEncoder, String password) {
+        return passwordEncoder.matches(password, this.password);
+    }
+
     protected User() {
     }
 
     public User(String userId, String password, String userName, String phoneNumber) {
         this.userId = userId;
+        this.password = password;
+        this.userName = userName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public User(String password, String userName, String phoneNumber, Long userId) {
         this.password = password;
         this.userName = userName;
         this.phoneNumber = phoneNumber;
--- src/main/java/com/example/team_project/domain/domain/user/service/purchase/UserPurchaseHistoryService.java
+++
@@ -1,9 +0,0 @@
-package com.example.team_project.domain.domain.user.service.purchase;
-
-import com.example.team_project.domain.domain.order.item.domain.Order;
-
-import java.util.List;
-
-public interface UserPurchaseHistoryService {
-    List<Order> getPurchaseHistoryList(String userId);
-}
