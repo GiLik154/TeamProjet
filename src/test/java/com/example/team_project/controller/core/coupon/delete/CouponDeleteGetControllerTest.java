a7e5da0f675d2e5da25ce16623e055bf6fa7a87a
gilik154 <okk080200@gmail.com>
2023-04-11 12:06:44 +0900

    feat : 컨트롤단 테스트코드 작성 완료

--- src/test/java/com/example/team_project/controller/core/coupon/delete/CouponDeleteGetControllerTest.java
+++ src/test/java/com/example/team_project/controller/core/coupon/delete/CouponDeleteGetControllerTest.java
@@ -1,17 +1,13 @@
-package com.example.team_project.controller.core.coupon.add;
+package com.example.team_project.controller.core.coupon.delete;
 
 import com.example.team_project.controller.core.coupon.delete.CouponDeleteController;
 import com.example.team_project.domain.domain.coupons.coupon.domain.Coupon;
 import com.example.team_project.domain.domain.coupons.coupon.domain.CouponRepository;
-import com.example.team_project.domain.domain.user.domain.User;
-import com.example.team_project.domain.domain.user.domain.UserRepository;
-import com.example.team_project.enums.UserGrade;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -19,25 +15,22 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @Transactional
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-class CouponDeletePostControllerTest {
+class CouponDeleteGetControllerTest {
     private final CouponDeleteController couponDeleteController;
     private final CouponRepository couponRepository;
-    private final UserRepository userRepository;
     private MockMvc mockMvc;
 
     @Autowired
-    CouponDeletePostControllerTest(CouponDeleteController couponDeleteController, CouponRepository couponRepository, UserRepository userRepository) {
+    CouponDeleteGetControllerTest(CouponDeleteController couponDeleteController, CouponRepository couponRepository) {
         this.couponDeleteController = couponDeleteController;
         this.couponRepository = couponRepository;
-        this.userRepository = userRepository;
     }
 
     @BeforeEach
@@ -47,26 +40,16 @@ class CouponDeletePostControllerTest {
     }
 
     @Test
-    void Post_쿠폰_삭제_정상출력() throws Exception {
-        User user = new User("testId", "testPw", "testNane", "testNumber");
-        user.updateUserGrade(UserGrade.VIP);
-        userRepository.save(user);
-        Long userId = user.getId();
-
-        MockHttpSession session = new MockHttpSession();
-        session.setAttribute("userId", userId);
-
+    void Get_쿠폰_삭제_정상출력() throws Exception {
         Coupon coupon = new Coupon("testName", 50, 10000, 1);
         couponRepository.save(coupon);
 
-        MockHttpServletRequestBuilder builder = post("/coupon/delete/" + coupon.getName())
-                .session(session);
-
-        mockMvc.perform(builder)
-                .andExpect(status().isOk());
+        MockHttpServletRequestBuilder builder = get("/coupon/delete");
 
         List<Coupon> couponList = couponRepository.findAll();
 
-        assertTrue(couponList.isEmpty());
+        mockMvc.perform(builder)
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("coupon_list", couponList));
     }
 }
