a8261fc94860ffbd48475b077c430f2ef3dcb7a3
Choisehun <wjsrkqeovh@naver.com>
2023-04-17 16:06:41 +0900

    수정
    -> 아이디 , 비밀번호 찾기
    -> 아이디는 오너이름,전화번호 로 검색함.
    -> 비밀번호는 아이디,이메일로 입력 해당 이메일로 토큰 발송 -> 토큰 검증후 통과시 비밀번호 변경 가능
    -> error/error-page 수정
    
    -> 이메일 사용시 application.properties 부분에서
    spring.mail.username 과 spinrg.mail.password 부분에 본인 네이버 메일 비밀번호 입력 필요함.

--- src/main/java/com/example/team_project/controller/core/product/ProductRegistrationController.java
+++ src/main/java/com/example/team_project/controller/core/product/ProductRegistrationController.java
@@ -22,7 +22,6 @@ public class ProductRegistrationController {
 
     @PostMapping("")
     public String registration(@SessionAttribute("sellerId") Long sellerId, ProductDto productDto, MultipartFile multipartFile) {
-        System.out.println("사진: " + multipartFile);
         productRegistrationService.productRegistration(sellerId, productDto, multipartFile);
 
         return "redirect:/product/registration";
