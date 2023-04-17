ea6255ba202a6773fda3a1d37519c8f3836f5c02
SeoljunLim <hk6131334@naver.com>
2023-04-17 17:42:07 +0900

    feat post , review 수정

--- src/main/java/com/example/team_project/controller/core/review/recommend/ReviewRecommendController.java
+++ src/main/java/com/example/team_project/controller/core/review/recommend/ReviewRecommendController.java
@@ -22,6 +22,6 @@ public class ReviewRecommendController {
             reviewRecommendUpdateService.update(userId,baseReviewId,trueOrFalse);
         }
 
-        return "redirect:/post/list";
+        return "redirect:/main";
     }
 }
