7db396a9ea24b531ffc89e28ceccf3dc454bef82
SeoljunLim <hk6131334@naver.com>
2023-04-19 11:20:04 +0900

    feat post , review 수정

--- src/main/java/com/example/team_project/domain/domain/user/service/purchase/CancelPurchaseServiceImpl.java
+++ src/main/java/com/example/team_project/domain/domain/user/service/purchase/CancelPurchaseServiceImpl.java
@@ -1,25 +1,25 @@
-package com.example.team_project.domain.domain.user.service.purchase;
-
-import com.example.team_project.domain.domain.order.item.domain.Order;
-import com.example.team_project.domain.domain.order.item.domain.OrderRepository;
-import com.example.team_project.domain.domain.order.list.domain.OrderList;
-import com.example.team_project.domain.domain.user.domain.User;
-import com.example.team_project.domain.domain.user.domain.UserRepository;
-import org.springframework.stereotype.Service;
-
-@Service
-public class CancelPurchaseServiceImpl implements CancelPurchaseService {
-    private final UserRepository userRepository;
-    private final OrderRepository orderRepository;
-
-    CancelPurchaseServiceImpl(UserRepository userRepository, OrderRepository orderRepository) {
-        this.userRepository = userRepository;
-        this.orderRepository = orderRepository;
-    }
-
-    @Override
-    public void cancelPurchase(Long userId, Order order, OrderList orderList) {
-//        User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("Invalid user ID"));
-
-    }
-}
+//package com.example.team_project.domain.domain.user.service.purchase;
+//
+//import com.example.team_project.domain.domain.order.item.domain.Order;
+//import com.example.team_project.domain.domain.order.item.domain.OrderRepository;
+//import com.example.team_project.domain.domain.order.list.domain.OrderList;
+//import com.example.team_project.domain.domain.user.domain.User;
+//import com.example.team_project.domain.domain.user.domain.UserRepository;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class CancelPurchaseServiceImpl implements CancelPurchaseService {
+//    private final UserRepository userRepository;
+//    private final OrderRepository orderRepository;
+//
+//    CancelPurchaseServiceImpl(UserRepository userRepository, OrderRepository orderRepository) {
+//        this.userRepository = userRepository;
+//        this.orderRepository = orderRepository;
+//    }
+//
+//    @Override
+//    public void cancelPurchase(Long userId, Order order, OrderList orderList) {
+////        User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("Invalid user ID"));
+//
+//    }
+//}
