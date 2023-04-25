dc0dab6d73690b50454cfb83f2f73df2371e05e9
rigel9327 <rigel9327@gmail.com>
2023-04-25 09:21:08 +0900

    작성중

--- src/main/java/com/example/team_project/security/oauth2/handler/OAuth2LoginSuccessHandler.java
+++ src/main/java/com/example/team_project/security/oauth2/handler/OAuth2LoginSuccessHandler.java
@@ -49,7 +49,6 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
 
     }
 
-    // TODO : 소셜 로그인 시에도 무조건 토큰 생성하지 말고 JWT 인증 필터처럼 RefreshToken 유/무에 따라 다르게 처리해보기
     private void loginSuccess(HttpServletResponse response, CustomOAuth2User oAuth2User , HttpSession session) throws IOException {
         String accessToken = jwtService.createAccessToken(oAuth2User.getUserId());
         String refreshToken = jwtService.createRefreshToken();
