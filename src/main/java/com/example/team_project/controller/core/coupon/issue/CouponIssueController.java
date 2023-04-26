1d49f1818c8bd71fc0ea0feb974b925db1160cb4
gilik154 <okk080200@gmail.com>
2023-04-27 00:04:22 +0900

    feat : "유저 주소 List, Add 만듬"

--- src/main/java/com/example/team_project/controller/core/coupon/issue/CouponIssueController.java
+++ src/main/java/com/example/team_project/controller/core/coupon/issue/CouponIssueController.java
@@ -2,6 +2,7 @@ package com.example.team_project.controller.core.coupon.issue;
 
 import com.example.team_project.domain.domain.coupons.usercoupon.service.issue.UserCouponIssueService;
 import com.example.team_project.domain.domain.coupons.usercoupon.service.issue.UserIssueAbleCouponListService;
+import com.example.team_project.domain.domain.user.domain.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,9 +14,13 @@ import org.springframework.web.bind.annotation.*;
 public class CouponIssueController {
     private final UserCouponIssueService userCouponIssueService;
     private final UserIssueAbleCouponListService userIssueAbleCouponListService;
+    private final UserRepository userRepository;
 
     @GetMapping
     public String get(@SessionAttribute Long userId, Model model) {
+        userRepository.findById(userId).ifPresent(user ->
+                model.addAttribute("userGrade", user.getUserGrade()));
+
         model.addAttribute("couponList", userIssueAbleCouponListService.getList(userId));
 
         return "thymeleaf/coupon/issue";
