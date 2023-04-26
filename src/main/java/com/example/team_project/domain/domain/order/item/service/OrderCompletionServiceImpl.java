8eedb9cf30bee950285d37d6903fe88701ad78bd
hyeok <cyjria7611@gmail.com>
2023-04-26 16:57:34 +0900

    order_payment.html 빼고 완료

--- src/main/java/com/example/team_project/domain/domain/order/item/service/OrderCompletionServiceImpl.java
+++ src/main/java/com/example/team_project/domain/domain/order/item/service/OrderCompletionServiceImpl.java
@@ -1,6 +1,8 @@
 package com.example.team_project.domain.domain.order.item.service;
 
+import com.example.team_project.domain.domain.order.item.domain.Order;
 import com.example.team_project.domain.domain.order.item.domain.OrderRepository;
+import com.example.team_project.domain.domain.order.item.domain.OrderToProduct;
 import com.example.team_project.domain.domain.order.list.service.OrderListUpdateService;
 import com.example.team_project.domain.domain.order.list.service.OrderStatusUpdateForPaymentsService;
 import com.example.team_project.enums.OrderStatus;
@@ -15,14 +17,19 @@ import javax.transaction.Transactional;
 public class OrderCompletionServiceImpl implements OrderCompletionService {
     private final OrderRepository orderRepository;
     private final OrderListUpdateService orderListUpdateService;
+    private final OrderStatusUpdateForPaymentsService orderStatusUpdateForPaymentsService;
 
     @Override
     public void processOrderPayment(Long userId, Long userAddressId, Long paymentId, Long orderListId) {
         updateOrderListInfo(userId, orderListId, userAddressId, paymentId);
         updateAllOrdersToPaid(userId, orderListId);
+        orderStatusUpdateForPaymentsService.update(orderListId);
+        updateProductSalesAndStock(orderListId);
     }
 
-    /** 해당 주문리스트안의 주문들 상태들 모두 결제완료상태로 업데이트 함 **/
+    /**
+     * 해당 주문리스트안의 주문들 상태들 모두 결제완료상태로 업데이트 함
+     **/
     private void updateAllOrdersToPaid(Long userId, Long orderListId) {
         orderRepository.findByUserIdAndOrderListId(userId, orderListId).forEach(order ->
                 order.getOrderToProduct().updateStatus(OrderStatus.PAYMENT_COMPLETED));
@@ -31,6 +38,13 @@ public class OrderCompletionServiceImpl implements OrderCompletionService {
 
     private void updateOrderListInfo(Long userId, Long orderListId, Long userAddressId, Long paymentId) {
         orderListUpdateService.update(userId, orderListId, userAddressId, paymentId);
+    }
+
+    private void updateProductSalesAndStock(Long orderListId) {
+        orderRepository.findListByOrderListId(orderListId).forEach(order -> {
+            OrderToProduct orderToProduct = order.getOrderToProduct();
+            orderToProduct.getProduct().increaseSalesCountAndDecreaseStock(orderToProduct.getQuantity());
+        });
     }
 
 }
